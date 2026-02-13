49375e254f71a70fa5ad3a96da4511afc63e9d44
Tibotsr <tibo.tessier@gmail.com>
2026-02-13 09:46:09 +0100

    Suppresion fonction en double

--- scripts/event_details_redirect.js
+++ scripts/event_details_redirect.js
@@ -1,26 +1,29 @@
 const eventDivs = document.querySelectorAll('.event');
 
-// Ajouter un gestionnaire de clic à chaque div
 eventDivs.forEach(div => {
     div.addEventListener('click', (event) => {
-        const eventId = div.getAttribute('event-id'); // Récupère l'ID de l'événement
+        const eventId = div.getAttribute('event-id');
+        const type = div.getAttribute('data-type') || 'event';
+        let url;
         if (eventId) {
-            const url = `event_details.php?id=${eventId}`;
+            if (type === 'news') {
+                url = `news_details.php?id=${eventId}`;
+            } else {
+                url = `event_details.php?id=${eventId}`;
+            }
 
             // Détecte le clic molette ou Ctrl/Cmd + clic
             if (event.ctrlKey || event.metaKey || event.button === 1) {
-                window.open(url, '_blank'); // Ouvre dans un nouvel onglet
+                window.open(url, '_blank');
             } else {
-                // Sinon, redirige normalement
-                window.location.href = url;
+                globalThis.location.href = url;
             }
         }
     });
 
-    // Empêche la sélection de texte sur le clic molette
     div.addEventListener('mousedown', (event) => {
         if (event.button === 1) {
-            event.preventDefault(); // Empêche le comportement par défaut
+            event.preventDefault();
         }
     });
 });
