85de8692e5d96fe780b1c6e82d1146ccc917ccd5
TomJ35 <137908479+TomJ35@users.noreply.github.com>
2026-04-07 16:42:06 +0200

    Delete .tmp_item1_json.php

--- .tmp_item1_json.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'api/DB.php';
-require_once 'api/models/Item.php';
-use model\Item;
-
-$item = Item::getInstance(1);
-header('Content-Type: application/json');
-echo json_encode($item, JSON_PRETTY_PRINT);
