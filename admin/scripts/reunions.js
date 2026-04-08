fc1db401d32ab59bf015de902c4d7d64f53b4df8
TomJ35 <tom.jego35@gmail.com>
2026-04-08 14:40:29 +0200

    ajout possibilite de changer la date des réunions

--- admin/scripts/reunions.js
+++ admin/scripts/reunions.js
@@ -1,5 +1,5 @@
 import { refreshNavbar } from "./navbar.js";
-import { requestGET, requestDELETE, requestPOST } from './ajax.js';
+import { requestGET, requestDELETE, requestPOST, requestPUT } from './ajax.js';
 import { showLoader, hideLoader } from "./loader.js";
 import { toast } from "./toaster.js";
 import { showPropertieSkeleton, hidePropertieSkeleton } from "./propertieskeleton.js";
@@ -13,6 +13,8 @@ const download_btn = document.getElementById('download_btn');
 const delete_btn = document.getElementById('delete_btn');
 const new_btn = document.getElementById('new_btn');
 const pdf_preview = document.getElementById('pdf_preview');
+const prop_date = document.getElementById('prop_date');
+const save_btn = document.getElementById('save_btn');
 
 /**
  * Reloads the navigation bar with reunions items.
@@ -67,6 +69,11 @@ async function selectReunion(id_reunion, li){
     // Fetch grade information
     const role = await requestGET(`/meeting.php?id=${id_reunion}`);
 
+    // Update date field
+    if (prop_date) {
+        prop_date.value = role.date_reunion.split(' ')[0];
+    }
+
     // Update displayed information
     const url = getFileBucketUrl(role.fichier_reunion);
     if (!url) {
@@ -99,6 +106,26 @@ async function selectReunion(id_reunion, li){
                 deleteReunion(id_reunion);
             }
           });
+    };
+
+    // Save date button
+    save_btn.onclick = async ()=> {
+        if (!prop_date.value) {
+            toast("Veuillez sélectionner une date.", true);
+            return;
+        }
+
+        showLoader();
+        try {
+            await requestPUT(`/meeting.php?id=${id_reunion}`, { date: prop_date.value });
+            if (li) {
+                li.textContent = prop_date.value;
+            }
+            toast('Date de réunion mise à jour avec succès.');
+        } catch (error) {
+            toast(error.message, true);
+        }
+        hideLoader();
     };
 
     // Hide loader
